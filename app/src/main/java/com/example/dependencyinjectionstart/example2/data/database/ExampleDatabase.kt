32ee22dd1d1f14f060d86c9a77fe71ccd3509cf7
YuriyPetukhov <pochtalosyahs@gmail.com>
2025-05-03 15:03:06 +0300

    11.8 Custom Builder, BindsInstance

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/database/ExampleDatabase.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/database/ExampleDatabase.kt
@@ -1,15 +1,21 @@
 package com.example.dependencyinjectionstart.example2.data.database
 
+import android.content.Context
 import android.util.Log
+import com.example.dependencyinjectionstart.R
 import javax.inject.Inject
 
-class ExampleDatabase @Inject constructor() {
+class ExampleDatabase @Inject constructor(
+    private val context: Context,
+    private val timeMillis: Long
+) {
 
     fun method() {
-        Log.d(LOG_TAG, "ExampleDatabase")
+        Log.d(LOG_TAG, "ExampleDatabase ${context.getString(R.string.app_name)} $timeMillis")
     }
 
     companion object {
+
         private const val LOG_TAG = "EXAMPLE_TEST"
     }
 }
--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example1.Activity
+import com.example.dependencyinjectionstart.example1.DaggerNewComponent
+import com.example.dependencyinjectionstart.example2.di.ApplicationComponent
 import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-
     @Inject
     lateinit var viewModel: ExampleViewModel
 
-    private val component = DaggerApplicationComponent.create()
+    private val component by lazy {
+        DaggerApplicationComponent.builder()
+            .context(application)
+            .timeMillis(System.currentTimeMillis())
+            .build()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         component.inject(this)
