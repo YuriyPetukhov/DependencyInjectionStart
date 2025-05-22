7ef9f31eaf3517301ab4d4f5d1c0fe0289d8691a
YuriyPetukhov <pochtalosyahs@gmail.com>
2025-05-22 15:28:49 +0300

    Создали свою Singleton-аннотацию и установили ее в Domain-модуле

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/DataModule.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/DataModule.kt
@@ -6,15 +6,14 @@ import com.example.dependencyinjectionstart.example2.data.datasource.ExampleRemo
 import com.example.dependencyinjectionstart.example2.data.datasource.ExampleRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 @Module
 interface DataModule {
-
+    @ApplicationScope
     @Binds
     fun bindLocalDataSource(impl: ExampleLocalDataSourceImpl): ExampleLocalDataSource
 
-
+    @ApplicationScope
     @Binds
     fun bindRemoteDataSource(impl: ExampleRemoteDataSourceImpl): ExampleRemoteDataSource
 
